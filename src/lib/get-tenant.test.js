fb8a284c873b8ff6f4ff9e8e310e294227d77c29
vsouza <vinicius.rj@gmail.com>
2017-06-18 11:48:21 -0300

    improve gettenant test

--- src/lib/get-tenant.test.js
+++ src/lib/get-tenant.test.js
@@ -2,7 +2,12 @@
 
 import getTenant from './get-tenant';
 
-test('', () => {
+test('parse a valid token', () => {
     let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJRCI6InRlc3RlIn0.j-NUJF8fbeV5fDtASOu3QZPXmoeo_tZFAHbGAwGbJe4";
     return getTenant('secret')([token, "A", 123, {"name": "B"}]).then(data => expect(data).toContain('teste'));
+});
+
+test('fail with invalid token', () => {
+    let token = "sss.ssd.rtv";
+    return getTenant('Asecret')([token, "A", 123, {"name": "B"}]).catch(err => expect(err).toHaveProperty('msg', 'Fail to verify request token'));
 });
